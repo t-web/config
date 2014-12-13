5ac319e6bee5369d10b897a932841bf3f6ee0c52
Andrew Smith <a.smith@silentworks.co.uk>
2014-12-13 22:46:34 +0000

    Updated tests for individual adapters

--- tests/FileTypeAdapter/IniAdapterTest.php
+++ tests/FileTypeAdapter/IniAdapterTest.php
@@ -1,14 +1,5 @@
 <?php
 
-$_EXAMPLE_INI = <<<EOF
-foo = bar
-
-[subfoo]
-baz = fwibble
-
-EOF;
-
-
 class IniAdapterTest extends PHPUnit_Framework_TestCase
 {
     public function testLoadFrom()
@@ -17,28 +8,25 @@ class IniAdapterTest extends PHPUnit_Framework_TestCase
         $this->assertInternalType('array', $adapter->loadFrom('foo'));
     }
 
-
     public function testParse()
     {
-        global $_EXAMPLE_INI;
         $adapter = new \Slender\Configurator\FileTypeAdapter\IniAdapter();
+        $result = $adapter->parse(__DIR__ . '/../config/config.ini');
 
-        // Create tmp file to load from
-        $tmpFile = sys_get_temp_dir().'/tmp-configurator.ini';
-        file_put_contents($tmpFile,$_EXAMPLE_INI);
+        $this->assertCount(2, $result);
 
-
-        $result = $adapter->parse($tmpFile);
-
-
-        $this->assertCount(2,$result);
-
-        $this->assertArrayHasKey('foo',$result);
+        $this->assertArrayHasKey('foo', $result);
         $this->assertEquals('bar', $result['foo']);
 
         $this->assertArrayHasKey('subfoo', $result);
         $this->assertCount(1, $result['subfoo']);
         $this->assertArrayHasKey('baz', $result['subfoo']);
         $this->assertEquals('fwibble', $result['subfoo']['baz']);
+        $this->assertEquals([
+            'foo' => 'bar',
+            'subfoo' => [
+                'baz' => 'fwibble',
+            ],
+        ], $result);
     }
 }
--- tests/FileTypeAdapter/JsonAdapterTest.php
+++ tests/FileTypeAdapter/JsonAdapterTest.php
@@ -7,4 +7,16 @@ class JsonAdapterTest extends PHPUnit_Framework_TestCase
         $adapter = new \Slender\Configurator\FileTypeAdapter\JsonAdapter();
         $this->assertInternalType('array', $adapter->loadFrom('foo'));
     }
+
+    public function testParse()
+    {
+        $adapter = new \Slender\Configurator\FileTypeAdapter\JsonAdapter();
+        $result = $adapter->parse(__DIR__ . '/../config/config.json');
+        $this->assertInternalType('array', $result);
+        $this->assertCount(2, $result);
+        $this->assertEquals([
+            'foo' => 'bar',
+            'abc' => 'baz',
+        ], $result);
+    }
 }
